88fcd7c5f3e5ac84c2c187ef41c6ff949e8da925
Giovanna Santana <gigisantanasilva@gmail.com>
2026-03-10 11:03:46 -0300

    10/03

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import {Timer, TimerIcon} from 'lucide-react'
+import styles from './styles.module.css';
+
+export function Logo(){
+    return(
+        <div>
+            <a href="#"></a>
+        </div>
+    )
+}
